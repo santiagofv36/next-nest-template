3b8f7d6d5f80f8f21671480afa8e2cd19526f4ea
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-07-28 13:50:37 -0400

    feat(admin): :sparkles: copying setup for admin from client

---
+++ apps/admin/src/lib/api.ts
@@ -0,0 +1,125 @@
+import Axios from 'axios';
+import { z } from 'zod';
+
+type TFetchInput<DataType> = {
+  url: string;
+  schema?: z.ZodType<DataType>;
+  options?: RequestInit;
+};
+
+export const fetchWrapper = async <DataType>({
+  url,
+  schema = z.any() as z.ZodType<DataType>,
+  options = {},
+}: TFetchInput<DataType>): Promise<TFetchOutput<DataType>> => {
+  const headers: HeadersInit = {};
+
+  // add content type if needed
+  if (options.body) {
+    headers['Content-Type'] = 'application/json';
+  }
+
+  // add access token if needed
+  if (typeof window !== 'undefined' && localStorage.getItem('token') !== null) {
+    headers['Authorization'] = localStorage.getItem('token')!;
+  }
+
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + url, {
+    ...options,
+    headers,
+  });
+
+  const data = await response?.json();
+
+  // VALIDATE RESPONSE WITH ZOD
+  try {
+    const parsedData = schema.parse(data);
+    return {
+      response,
+      data: parsedData,
+    };
+  } catch (error) {
+    // puede manejar este error como desee
+    throw new Error(`Invalid response structure: ${error}`);
+  }
+};
+
+type TFetchOutput<DataType> = {
+  response: Response;
+  data: DataType;
+};
+
+export const api = {
+  get: async <DataType>({
+    url,
+    schema = z.any() as z.ZodType<DataType>,
+    options = {},
+  }: TFetchInput<DataType>) =>
+    fetchWrapper({
+      url,
+      schema,
+      options: {
+        ...options,
+        method: 'GET',
+      },
+    }),
+
+  post: async <DataType>({
+    url,
+    schema = z.any() as z.ZodType<DataType>,
+    options = {},
+  }: TFetchInput<DataType>) =>
+    fetchWrapper({
+      url,
+      schema,
+      options: {
+        ...options,
+        method: 'POST',
+      },
+    }),
+
+  put: async <DataType>({
+    url,
+    schema = z.any() as z.ZodType<DataType>,
+    options = {},
+  }: TFetchInput<DataType>) =>
+    fetchWrapper({
+      url,
+      schema,
+      options: {
+        ...options,
+        method: 'PUT',
+      },
+    }),
+
+  delete: async <DataType>({
+    url,
+    schema = z.any() as z.ZodType<DataType>,
+    options = {},
+  }: TFetchInput<DataType>) =>
+    fetchWrapper({
+      url,
+      schema,
+      options: {
+        ...options,
+        method: 'DELETE',
+      },
+    }),
+};
+
+export const axios = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'x-access-token':
+      typeof window !== 'undefined' ? localStorage.getItem('token') : undefined,
+  },
+});
+
+export const axiosS3 = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'x-amz-acl': 'public-read',
+  },
+});
---
+++ apps/admin/src/lib/auth.ts
@@ -0,0 +1,38 @@
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '', // TODO: Add the path to your sign in page
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+          // Call backend and set the token in local storage
+        } catch (e) {
+          console.error(e);
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token.user) session.user = token.user as any;
+      return session;
+    },
+  },
+};
